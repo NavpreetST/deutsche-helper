43e5fb13c933cc68a1aa6dc8c075e4eee0c4e413
NavpreetST <999navtoor@gmail.com>
2025-07-25 20:05:34 +0200

    Removed the default template chat

--- src/app/page.js
+++ src/app/page.js
@@ -7,8 +7,11 @@ import SideBar from "./components/SideBar"
 function App() {
 
   return (
-   <div className='text-shadow-purple-950 border-2 bg-amber-500  text-2xl w-max h-max min-w-12/12'>
-    fucker
+   <div className='text-shadow-purple-950 border-2 bg-amber-500  text-2xl w-11/12 h-max min-w-12/12'>
+    <div className="font-bold flex items-center justify-center ">
+    Teletraan-1
+
+    </div>
     <ChatWindow/>
 
     <SideBar/>
